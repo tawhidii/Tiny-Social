f9279b1d435dbad078c5785dc6c0bdd3cc0d282d
Khondoker Khademul Bari <barii.py@gmail.com>
2021-01-31 21:27:34 +0600

    Added user registration to accounts

--- account/froms.py
+++ account/froms.py
@@ -1,7 +1,23 @@
+from django.contrib.auth.models import User
 from django import forms
+
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat Password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'email')
+
+    # MATCH PASSWORD
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Password do not match')
+        return cd['password2']
+
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
-
-
--- account/views.py
+++ account/views.py
@@ -1,10 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.contrib.auth import authenticate,login
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from .froms import LoginForm
+from .froms import LoginForm, UserRegistrationForm
 
-# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+
+            return render(request, 'account/register_done.html', {'new_user': new_user})
+
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'account/register.html', {'user_form': user_form})
+
 
 def user_login(request):
     if request.method == "POST":
@@ -16,8 +30,8 @@ def user_login(request):
                                 password=cd['password'])
             if user is not None:
                 if user.is_active:
-                    login(request,user)
-                    return HttpResponse('Authenticated','successfully')
+                    login(request, user)
+                    return HttpResponse('Authenticated', 'successfully')
                 else:
                     return HttpResponse('Disabled Account')
             else:
@@ -26,13 +40,12 @@ def user_login(request):
     else:
         form = LoginForm()
 
-    return render(request,'account/login.html',{'form': form })
-    
+    return render(request, 'account/login.html', {'form': form})
+
 
 @login_required
 def dashboard(request):
     context = {
-        'section':'dashboard'
+        'section': 'dashboard'
     }
-    return render(request,'account/dashboard.html',context)
-
+    return render(request, 'account/dashboard.html', context)
